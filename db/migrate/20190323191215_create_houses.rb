57a6ac86dcb2f4a93119be282f535f993106cf54
Erick Cordeiro Ozaki <erick.cordeiro@locaweb.com.br>
2019-03-23 16:48:06 -0300

    Houses index

--- db/migrate/20190323191215_create_houses.rb
+++ db/migrate/20190323191215_create_houses.rb
@@ -1,7 +1,7 @@
 class CreateHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :houses do |t|
-      t.float :price, limit: 2
+      t.float :price, precision: 2
       t.text :address
       t.text :description
 
