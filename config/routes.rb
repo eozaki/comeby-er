5b825a0630744e58476359e645240b5fa988019a
Erick Cordeiro Ozaki <erick.cordeiro@locaweb.com.br>
2019-03-23 16:24:19 -0300

    Creating houses controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :agents
   devise_for :buyers, path: 'users'
   devise_for :admins
+  
+  resources :admins do
+    collection do
+      resources :houses
+    end
+  end
+
+  resources :houses, only: [:index, :show]
 end
